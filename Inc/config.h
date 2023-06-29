bc693485d207da9e8cdf7304883e9eec40e7876a
Donny36050 <137773921+Donny36050@users.noreply.github.com>
2023-06-29 19:28:04 +0200

    Update config.h atest

--- Inc/config.h
+++ Inc/config.h
@@ -8,8 +8,8 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 #include "stdint.h"
-//define DISPLAY_TYPE_EBiCS (1<<5)                  // King-Meter 618U protocol (KM5s, EBS-LCD2, J-LCD, SW-LCD)
-//#define DISPLAY_TYPE_KINGMETER_618U (1<<3)                  // King-Meter 618U protocol (KM5s, EBS-LCD2, J-LCD, SW-LCD)
+#define DISPLAY_TYPE_EBiCS (1<<5)                  // King-Meter 618U protocol (KM5s, EBS-LCD2, J-LCD, SW-LCD)
+#define DISPLAY_TYPE_KINGMETER_618U (1<<3)                  // King-Meter 618U protocol (KM5s, EBS-LCD2, J-LCD, SW-LCD)
 //#define DISPLAY_TYPE_KINGMETER_901U (1<<4)                  // King-Meter 901U protocol (KM5s)
 //#define DISPLAY_TYPE_KINGMETER      (DISPLAY_TYPE_KINGMETER_618U|DISPLAY_TYPE_KINGMETER_901U)
 #define DISPLAY_TYPE_BAFANG (1<<2)							// For 'Blaupunkt' Display of Prophete Entdecker
@@ -37,15 +37,15 @@
 #define I_FACTOR_I_D 2
 #define P_FACTOR_SPEED 1
 #define I_FACTOR_SPEED 10
-//#define TS_COEF 16000000
+#define TS_COEF 16000000
 #define PAS_TIMEOUT 8000
 #define RAMP_END 1200
-//#define THROTTLE_OFFSET 900
-//#define THROTTLE_MAX 2850
+#define THROTTLE_OFFSET 900
+#define THROTTLE_MAX 2850
 #define WHEEL_CIRCUMFERENCE 2600
 #define GEAR_RATIO 11 //11 for BionX IGH3
 #define SPEEDLIMIT 30
-#define PULSES_PER_REVOLUTION 10
+#define PULSES_PER_REVOLUTION 1
 #define PH_CURRENT_MAX 600
 #define BATTERYCURRENT_MAX 15000
 #define SPEC_ANGLE -167026406L //Value no longer needed
@@ -63,18 +63,18 @@
 //#define DISABLE_DYNAMIC_ADC
 //#define INDIVIDUAL_MODES
 //#define SPEEDTHROTTLE
-//#define THROTTLE_OVERRIDE
+#define THROTTLE_OVERRIDE
 #define REGEN_CURRENT_MAX 10000
 
 
 #define P_FACTOR_PLL 10
 #define I_FACTOR_PLL 10
 #define AUTODETECT 1
-#define SPEED_PLL 1 //1 for using PLL, 0 for angle extrapolation
+#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
 
 #define EXTERNAL 1
 #define INTERNAL 0
-#define SPEEDSOURCE EXTERNAL
+#define SPEEDSOURCE INTERNAL
 #define SPEEDFILTER 1
 #define SIXSTEPTHRESHOLD 20000
 
@@ -82,6 +82,7 @@
 //#define ADC_BRAKE
 
 #endif /* CONFIG_H_ */
+
 
 
 
